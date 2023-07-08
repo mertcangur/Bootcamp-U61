008d1a3216da4f9ae6995ff2804b3897ed842998
Sabriye Nur <79248086+sabisay@users.noreply.github.com>
2023-07-09 00:15:48 +0300

    screwMechanism

--- Assets/Brick Project Studio/_BPS Basic Assets/Common/Scripts and Animations/Doors/opencloseDoor.cs
+++ Assets/Brick Project Studio/_BPS Basic Assets/Common/Scripts and Animations/Doors/opencloseDoor.cs
@@ -1,22 +1,37 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 	public class opencloseDoor : MonoBehaviour
 	{
 
 		public Animator openandclose;
+		public Animator screwing;
 		public Transform Player;
 		public Collider doorColl;
 		public bool open = false;
+		public TMP_Text fButton;
 
 		void Start()
 		{
+			fButton.enabled = false;
 			openandclose.SetBool("Click", open);
 			doorColl.enabled = !enabled;
 		}
 		void Update(){
-			if(Input.GetKeyDown(KeyCode.F)){
-				StartCoroutine(Wait());
+			float dist = Vector3.Distance(Player.position, transform.position);
+			if(Player){
+				if( dist < 5f){
+					fButton.enabled = true;
+					if(Input.GetKeyDown(KeyCode.F)){
+					screwing.SetBool("Screwing", true);
+					StartCoroutine(Wait());
+
+				}else{
+					//fButton.enabled = false;
+				}
+			}
+			
 			}
 		}
 
