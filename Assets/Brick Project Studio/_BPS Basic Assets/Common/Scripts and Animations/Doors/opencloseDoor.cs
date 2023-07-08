73ce2824774596a8be610cea8de8030cf49b3aa4
Sabriye Nur <79248086+sabisay@users.noreply.github.com>
2023-07-08 22:52:08 +0300

    openandclose door

--- Assets/Brick Project Studio/_BPS Basic Assets/Common/Scripts and Animations/Doors/opencloseDoor.cs
+++ Assets/Brick Project Studio/_BPS Basic Assets/Common/Scripts and Animations/Doors/opencloseDoor.cs
@@ -1,72 +1,34 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
-namespace SojaExiles
-
-{
 	public class opencloseDoor : MonoBehaviour
 	{
 
 		public Animator openandclose;
-		public bool open;
 		public Transform Player;
+		public Collider doorColl;
+		public bool open = false;
 
 		void Start()
 		{
-			open = false;
+			openandclose.SetBool("Click", open);
+			doorColl.enabled = !enabled;
 		}
-
-		void OnMouseOver()
-		{
-			{
-				if (Player)
-				{
-					float dist = Vector3.Distance(Player.position, transform.position);
-					if (dist < 15)
-					{
-						if (open == false)
-						{
-							if (Input.GetMouseButtonDown(0))
-							{
-								StartCoroutine(opening());
-							}
-						}
-						else
-						{
-							if (open == true)
-							{
-								if (Input.GetMouseButtonDown(0))
-								{
-									StartCoroutine(closing());
-								}
-							}
-
-						}
-
-					}
-				}
-
+		void Update(){
+			if(Input.GetKeyDown(KeyCode.F)){
+				StartCoroutine(Wait());
 			}
-
 		}
 
-		IEnumerator opening()
-		{
-			print("you are opening the door");
-			openandclose.Play("Opening");
-			open = true;
-			yield return new WaitForSeconds(.5f);
-		}
-
-		IEnumerator closing()
-		{
-			print("you are closing the door");
-			openandclose.Play("Closing");
-			open = false;
-			yield return new WaitForSeconds(.5f);
-		}
+		void OnMouseDown()
+		{	
+			open = !open;
+			openandclose.SetBool("Click", open);
+			}
+			IEnumerator Wait() {
+				yield return new WaitForSeconds (3);
+				doorColl.enabled = enabled;
+			}
 
 
 	}
-}
