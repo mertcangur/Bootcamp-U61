e0774f91c38c04a087a74c89482a2ea190834042
murataydemir9 <119438420+murataydemir9@users.noreply.github.com>
2023-07-10 00:49:53 +0300

    Start Button Active Now

---
+++ Assets/Scripts/StartSceneStartIcon.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class StartSceneStartIcon : MonoBehaviour
+{
+   public void StartButton()
+    {
+        SceneManager.LoadScene("Sýgýnak");
+    }
+}
